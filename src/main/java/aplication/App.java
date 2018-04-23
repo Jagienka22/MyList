83cb6f631e4a0eea891f1a0304f6d31b26998b4f
Agnieszka Cwiekowska <agnieszka.cwiekowska@uj.edu.pl>
2018-04-23 22:02:17 +0200

    komendy: add, get, remove, contains, indexOf, isEmpty

---
+++ src/main/java/aplication/App.java
@@ -0,0 +1,68 @@
+package aplication;
+
+import list.MyList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aga on 25.03.18.
+ */
+public class App {
+
+    private static List<Integer> javaList;
+    private static MyList mojaLista;
+    private static MyList nowalista;
+
+    public static void main(String[] args) {
+        //MyList lista = new MyList();
+        //lista.add(5);
+        //System.out.println(lista);
+        javaList = new ArrayList<Integer>();
+        mojaLista = new MyList();
+        nowalista = new MyList();
+
+        dodaj(12);
+        dodaj(2);
+        dodaj(4);
+        dodaj(7);
+        dodaj(9);
+        dodaj(5);
+        dodaj(1);
+
+        System.out.println("Javowa lista: " + javaList);
+        System.out.println("Moja lista: " + mojaLista);
+
+        System.out.println(javaList.get(2));
+        System.out.println(mojaLista.get(2));
+
+        System.out.println(javaList.get(5));
+        System.out.println(mojaLista.get(5));
+        //zrób żeby twoja lista wypisywała to samo co java'owa
+
+        javaList.remove(4);
+        mojaLista.remove(4);
+
+        System.out.println("Javowa lista: " + javaList);
+        System.out.println("Moja lista: " + mojaLista);
+        //zrób żeby twoja lista wypisywała to samo co java'owa
+
+        javaList.addAll(javaList);
+        System.out.println("tttt" + javaList);
+
+        System.out.println("empty : " + mojaLista.isEmpty());
+        System.out.println(nowalista.isEmpty());
+        System.out.println(javaList.isEmpty());
+
+        System.out.println("contains: " + javaList.contains(14));
+        System.out.println(mojaLista.contains(14));
+
+        System.out.println("indexOf: " + javaList.indexOf(9));
+        System.out.println(mojaLista.indexOf(9));
+    }
+
+    private static void dodaj(int e) {
+        javaList.add(e);
+        mojaLista.add(e);
+    }
+}
---
+++ src/main/java/list/MyList.java
@@ -0,0 +1,70 @@
+package list;
+
+/**
+ * Created by aga on 25.03.18.
+ */
+public class MyList {
+    private int tablica[] = new int[100];
+    private int index;
+
+    public void add(int i) {
+        if (index == tablica.length) {
+            powiekszTablice();
+        }
+        tablica[index] = i;
+        index++;
+
+    }
+
+    public int get(int i) {
+        return tablica[i];
+    }
+
+    public void remove(int i) {
+        for (int j = i; j < index - 1; j++) {
+            tablica[j] = tablica[j + 1];
+        }
+        index--;
+    }
+
+    private void powiekszTablice() {
+        int[] tablica2 = new int[(tablica.length) + 100];
+        for (int j = 0; j < index; j++) {
+            tablica2[j] = tablica[j];
+        }
+        tablica = tablica2;
+    }
+
+    public boolean contains(int o) {
+        return indexOf(o) != -1;
+    }
+
+    public int indexOf(int o) {
+        for (int j = 0; j < index - 1; j++) {
+            if (tablica[j] == o) {
+                return j;
+            }
+        }
+        return -1;
+
+    }
+
+    public boolean isEmpty() {
+        return index == 0;
+    }
+
+    @Override
+    public String toString() {
+        String wynik = "[";
+        for (int w = 0; w < index - 1; w++) {
+            wynik = wynik + tablica[w] + ", ";
+        }
+        if (index != 0) {
+            wynik = wynik + tablica[index - 1];
+        }
+        return wynik + "]";
+    }
+}
+
+
+//usuwanie elementów z listy
